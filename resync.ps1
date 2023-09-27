695c961fad783fc37ea27bf3ccd7cae481db8a11
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2023-09-26 23:48:33 -0300

    wip

--- resync.ps1
+++ resync.ps1
@@ -1,9 +1,8 @@
 $prompt = Read-Host -Prompt "Deseja puxar(p) ou enviar(e)?";
-$iCloud = "$($env:USERPROFILE)\iCloudDrive\iCloud~md~obsidian\docs";
 
 if ($prompt -eq "p") {
-  Copy-Item -Force -Recurse "$iCloud" ".";
+  Copy-Item -Force -Recurse "$($env:USERPROFILE)\iCloudDrive\iCloud~md~obsidian\docs" ".\";
 }
 if ($prompt -eq "e") {
-  Copy-Item -Force -Recurse ".\docs" "$iCloud";
+  Copy-Item -Force -Recurse ".\docs" "$($env:USERPROFILE)\iCloudDrive\iCloud~md~obsidian\";
 }
