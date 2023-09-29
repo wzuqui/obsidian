3f4f15081d11cbe67240f0550f92fea2cdd02c5c
Willian Zuqui <willian.zuqui@ravex.com.br>
2023-09-29 12:59:52 -0300

    chore(obsidian): criado novo vault

--- resync.ps1
+++ resync.ps1
@@ -1,14 +1,19 @@
 $prompt = Read-Host -Prompt "Deseja puxar(p) ou enviar(e)?";
 
 Set-Location -Path ".\docs";
-
-
 if ($prompt -eq "e") {
   Copy-Item -Force -Recurse ".\" "$($env:USERPROFILE)\iCloudDrive\iCloud~md~obsidian\";
 }
-
 if ($prompt -eq "p") {
   Copy-Item -Force -Recurse "$($env:USERPROFILE)\iCloudDrive\iCloud~md~obsidian\docs" "..\";
 }
+Set-Location -Path "..";
 
+Set-Location -Path ".\blog";
+if ($prompt -eq "e") {
+  Copy-Item -Force -Recurse ".\" "$($env:USERPROFILE)\iCloudDrive\iCloud~md~obsidian\";
+}
+if ($prompt -eq "p") {
+  Copy-Item -Force -Recurse "$($env:USERPROFILE)\iCloudDrive\iCloud~md~obsidian\blog" "..\";
+}
 Set-Location -Path "..";
